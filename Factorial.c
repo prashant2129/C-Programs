234601ee6eb16ef75ee70963bd1dfed76bd964d0
Prashant Rakh <prashantrakh38@gmail.com>
2025-08-06 11:03:16 +0530

    Create Factorial.c

---
+++ Factorial.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int add_numbers(int a,int b) {
+    return a + b; }
+    int main() {
+        int result= add_numbers(3,5);
+        printf("Sum: %d\n",result);
+        return 0;}
